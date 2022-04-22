89b844c89fbbd8e39988abb9a206c7d39e31fa22
MizunoKazuki <mizukazu.work@gmail.com>
2022-04-22 16:34:16 +0900

    update: UIの変更

--- pages/api/sample.js
+++ pages/api/sample.js
@@ -4,5 +4,14 @@ export default function handler(req, res) {
     { name: 'sample', age: '32', flag: false },
     { name: 'aaaaa', age: '18', flag: false },
   ]
-  res.status(200).json(json);
+  // let jsonArr = [];
+  // for(let i=0; i<=100000; i++) {
+  //   jsonArr.push({
+  //     id: i,
+  //     name: `test${i}`,
+  //   });
+  // }
+  setTimeout(() => {
+    res.status(200).json(json);
+  }, 3000);
 }
--- pages/apiTest.js
+++ pages/apiTest.js
@@ -1,4 +1,6 @@
 import Container from '@mui/material/Container';
+import { Alert, Button } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 import ToTop from '../components/ToTop';
 import useSWR from 'swr';
 import { useQuery, QueryCache, ReactQueryCacheProvider, useQueryClient } from 'react-query';
@@ -6,25 +8,33 @@ import { useQuery, QueryCache, ReactQueryCacheProvider, useQueryClient } from 'r
 export default function Api() {
   const queryCache = new QueryCache();
   return (
-    <ReactQueryCacheProvider queryQache={queryCache}>
+    // <ReactQueryCacheProvider queryQache={queryCache}>
       <Container>
         <h1>APIテストページ</h1>
         <ToTop></ToTop>
-        <div>
-          { ReactQueryFetch() }
+        <div className="mt-3">
+          <GetAPI></GetAPI>
+          {/* <Button variant="contained" onClick={GetAPI}>GET</Button> */}
         </div>
       </Container>
-    </ReactQueryCacheProvider>
+    // </ReactQueryCacheProvider>
   )
 }
 
 function GetAPI() {
   const { data, error } = useSWR('/api/sample', (ep) => fetch(ep).then(res => res.json()));
   console.log(data);
-  if(error) return <div>failed to load</div>
-  if(!data) return <div>loading...</div>
+  if(error) return <Alert severity="error">エラーが発生しました。</Alert>
+  if(!data) {
+    return (
+      <>
+        Loading...
+        <CircularProgress />
+      </>
+    )
+  }
   const dom = data.map(d => {{ <div>{d.name}</div> }});
-  return dom
+  return <div data={data}>Hello { data[0].name }</div>
 }
 
 function ReactQueryFetch() {
